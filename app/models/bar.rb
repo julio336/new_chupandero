284d6428c8139db7c9cd04fcfcedfd5911b6cf53
julio <julio336@hotmail.com>
2013-04-01 20:04:33 -0600

    images

--- app/models/bar.rb
+++ app/models/bar.rb
@@ -1,5 +1,5 @@
 class Bar < ActiveRecord::Base
-  attr_accessible :description, :geo_address, :horary, :image1, :image2, :latitude, :logo,
+  attr_accessible :description, :geo_address, :horary, :image1, :image2, :image3, :image4, :latitude, :logo,
   				   :longitude, :name, :page, :phone, :promo, :address, :email
 
   geocoded_by :geo_address
